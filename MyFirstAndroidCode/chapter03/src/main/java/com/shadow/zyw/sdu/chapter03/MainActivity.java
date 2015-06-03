a10b9ded579c7add656d6cd8ada2fa725f65a0a3
shadow <zyw19871007@163.com>
2015-06-03 17:34:50 +0800

    myview

--- MyFirstAndroidCode/chapter03/src/main/java/com/shadow/zyw/sdu/chapter03/MainActivity.java
+++ MyFirstAndroidCode/chapter03/src/main/java/com/shadow/zyw/sdu/chapter03/MainActivity.java
@@ -1,17 +1,83 @@
 package com.shadow.zyw.sdu.chapter03;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+    private TextView tv;
+    private EditText et;
+    private Button btn;
+    private ProgressBar pb;
+    private Button btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.main_textv);
+        et = (EditText) findViewById(R.id.main_et);
+        btn = (Button) findViewById(R.id.main_btn);
+        btn2 = (Button) findViewById(R.id.main_btn2);
+        pb=(ProgressBar)findViewById(R.id.main_pb);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText(tv.getText().toString() + et.getText());
+                int progress = pb.getProgress();
+                progress+=10;
+                pb.setProgress(progress);
+                AlertDialog.Builder ad = new AlertDialog.Builder(MainActivity.this);
+                ad.setTitle("biaoti");
+                ad.setMessage("msg:hello");
+
+                ad.setCancelable(false);
+                ad.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        Toast.makeText(MainActivity.this, "cancle_true",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                ad.setNegativeButton("cancle_la", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "canclecancle",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                ad.setPositiveButton("im sure", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "okokok", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                ad.show();
+
+
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProgressDialog pd = new ProgressDialog(MainActivity.this);
+                pd.setTitle("title");
+                pd.setMessage("loading");
+                pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                pd.setProgress(12);
+                pd.show();
+                pd.setProgress(12);
+            }
+        });
     }
 
     @Override
