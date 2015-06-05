eb549b2a530f6872e368b8cb9edd22fcb621f025
shadow <zyw19871007@163.com>
2015-06-05 11:44:36 +0800

    customize layout

--- MyFirstAndroidCode/chapter03/src/main/java/com/shadow/zyw/sdu/chapter03/customize_layout.java
+++ MyFirstAndroidCode/chapter03/src/main/java/com/shadow/zyw/sdu/chapter03/customize_layout.java
@@ -1,18 +1,28 @@
 package com.shadow.zyw.sdu.chapter03;
 
+import android.app.Activity;
 import android.content.Context;
-import android.support.v4.view.LayoutInflaterFactory;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 /**
  * Created by shadow on 2015/6/4.
  */
 public class customize_layout extends LinearLayout {
-    public customize_layout(Context context, AttributeSet attrs) {
+    public customize_layout(final Context context, AttributeSet attrs) {
         super(context, attrs);
         LayoutInflater.from(context).inflate(R.layout.customize_layout, this);
+        Button btn = (Button) findViewById(R.id.cus_btn);
+        btn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((Activity)getContext()).finish();
+            }
+        });
 
     }
 }
