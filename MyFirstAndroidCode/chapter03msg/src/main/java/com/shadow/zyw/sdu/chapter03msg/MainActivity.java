0e0a789e47cf5a50c889bcdec86a0b79fbf8152e
shadow <zyw19871007@163.com>
2015-06-09 11:50:23 +0800

    chapter03 msg finish

--- MyFirstAndroidCode/chapter03msg/src/main/java/com/shadow/zyw/sdu/chapter03msg/MainActivity.java
+++ MyFirstAndroidCode/chapter03msg/src/main/java/com/shadow/zyw/sdu/chapter03msg/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -11,13 +14,17 @@ import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+    private EditText editText;
+    private Button btn;
+    private List<Msg> list = new ArrayList<>();
+    private ListView listView;
+    private MsgAdapter msgAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String data[] = {"a", "a1", "f", "b", "c", "sfa", "xvx",};
-        List<Msg> list = new ArrayList<>();
         Msg msg = new Msg("a",Msg.RECEIVE);
         list.add(msg);
         msg = new Msg("b", Msg.SEND);
@@ -28,9 +35,24 @@ public class MainActivity extends ActionBarActivity {
         list.add(msg);
         msg = new Msg("kjga", Msg.RECEIVE);
         list.add(msg);
-        ListView listView = (ListView) findViewById(R.id.listview);
-        MsgAdapter msgAdapter = new MsgAdapter(this, R.layout.list_item,list);
+        listView = (ListView) findViewById(R.id.listview);
+        msgAdapter = new MsgAdapter(this, R.layout.list_item,list);
         listView.setAdapter(msgAdapter);
+
+        editText = (EditText) findViewById(R.id.msg_edit);
+        btn = (Button) findViewById(R.id.msg_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = String.valueOf(editText.getText());
+                Msg msg1 = new Msg(str,Msg.SEND);
+                list.add(msg1);
+                msgAdapter.notifyDataSetChanged();
+                editText.setText("");
+                listView.setSelection(list.size());
+
+            }
+        });
 
     }
 
