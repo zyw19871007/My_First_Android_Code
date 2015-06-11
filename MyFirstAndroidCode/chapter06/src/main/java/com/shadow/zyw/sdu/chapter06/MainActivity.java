d12f5a055d8aaae01b39ef2e21edc06f57372be5
shadow <zyw19871007@163.com>
2015-06-11 17:44:45 +0800

    chapter 06 file  save load

--- MyFirstAndroidCode/chapter06/src/main/java/com/shadow/zyw/sdu/chapter06/MainActivity.java
+++ MyFirstAndroidCode/chapter06/src/main/java/com/shadow/zyw/sdu/chapter06/MainActivity.java
@@ -2,16 +2,82 @@ package com.shadow.zyw.sdu.chapter06;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private EditText editText;
+
+    private String load(String filename) {
+        FileInputStream fileInputStream = null;
+        BufferedReader bufferedReader = null;
+        StringBuffer stringBuffer = new StringBuffer();
+        try {
+            fileInputStream = openFileInput(filename);
+            bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+            String temp;
+            while ((temp = bufferedReader.readLine()) != null) {
+                stringBuffer.append(temp);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return String.valueOf(stringBuffer);
+    }
+    private void save(String str) {
+
+        FileOutputStream fileOutputStream = null ;
+        BufferedWriter writer = null;
+        try {
+            fileOutputStream = openFileOutput("data", MODE_PRIVATE);
+            writer = new BufferedWriter(new OutputStreamWriter(fileOutputStream));
+            writer.write(str);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+
+        }
+        finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        editText = (EditText) findViewById(R.id.main_edit);
+        save(String.valueOf(editText.getText()));
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.main_edit);
+        editText.setText(load("data"));
+        Log.e("test", String.valueOf(TextUtils.isEmpty(load("data"))));
     }
 
     @Override
@@ -20,6 +86,7 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
