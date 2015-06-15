d45197755b0461f914df975a2a5b26e8dfa20c8d
shadow <zyw19871007@163.com>
2015-06-15 17:44:33 +0800

    chapter09 first

--- MyFirstAndroidCode/chapter08/src/main/java/com/shadow/zyw/sdu/chapter08/MainActivity.java
+++ MyFirstAndroidCode/chapter08/src/main/java/com/shadow/zyw/sdu/chapter08/MainActivity.java
@@ -1,17 +1,38 @@
 package com.shadow.zyw.sdu.chapter08;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.notice);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NoticeViewActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0,intent,PendingIntent.FLAG_CANCEL_CURRENT);
+
+                NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                Notification notification = new Notification.Builder(MainActivity.this).setContentTitle("title").setContentText("lala,alala").setSmallIcon(R.mipmap.ic_launcher).setContentIntent(pendingIntent).build();
+                notificationManager.notify(1, notification);
+
+
+            }
+        });
     }
 
     @Override
